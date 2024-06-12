116f0e8cb2ad3b5c58cdac100f5c74f17318b362
griselda4774 <trandinhtam4774@gmail.com>
2024-06-13 00:43:38 +0700

    view noti customer, view order admin

--- app/(landing)/customer/notification/page.js
+++ app/(landing)/customer/notification/page.js
@@ -12,12 +12,25 @@ import trimText from "@/utils/trimText";
 import { CustomViewDialog } from "@/components/custom/Admin/Dialog/CustomViewDialog";
 import iconEye from "@/public/ic_eye.svg";
 import { CustomViewNoti } from "@/components/custom/Customer/CustomViewNoti";
+import iconDiscount from "@/public/ic_profile/ic_discount.svg";
 
 const NotificationPage = () => {
   const [notificationList, setNotificationList] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(8);
   const [totalItems, setTotalItems] = useState(0);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    return formattedDate;
+  };
 
   const getNotificationData = async () => {
     const data = await getAllNotifications(currentPage, itemsPerPage);
@@ -57,15 +70,21 @@ const NotificationPage = () => {
                       {trimText(item.title, 24)}
                     </div>
                     <div
-                      style={{ width: "70%" }}
                       className="flex flex-row items-center justify-between"
+                      style={{ width: "70%" }}
                     >
                       <div
-                        className="text-gray-400 text-sm"
+                        className="flex flex-col items-start justify-start"
                         style={{ width: "90%" }}
                       >
-                        {item.message}
+                        <div className="text-gray-400 text-xs italic w-full">
+                          {formatDate(item.timestamp)}
+                        </div>
+                        <div className="text-gray-400 text-sm w-full">
+                          {item.message}
+                        </div>
                       </div>
+
                       <div className="cursor-pointer">
                         <CustomViewNoti
                           itemTrigger={
@@ -78,9 +97,20 @@ const NotificationPage = () => {
                           title={"THÔNG BÁO"}
                           itemContent={
                             <>
-                              <div className="flex flex-col items-start justify-start w-full">
-                                <div className="mt-[8px] flex flex-row items-start justify-start w-full">
-                                  <div className="flex flex-row items-start justify-start w-full"></div>
+                              <div className="flex flex-col items-center justify-center w-full">
+                                <Image
+                                  src={iconDiscount}
+                                  width={120}
+                                  height={120}
+                                />
+                                <div className="text-center text-lg font-semibold text-black mt-[20px] w-full">
+                                  {item.title}
+                                </div>
+                                <div className="text-center text-xs italic text-gray-400 w-full">
+                                  {formatDate(item.timestamp)}
+                                </div>
+                                <div className="text-center text-sm text-gray-600 mt-[20px] w-full">
+                                  {item.message}
                                 </div>
                               </div>
                             </>
--- services/userServices.js
+++ services/userServices.js
@@ -1,22 +1,24 @@
 import axios from "axios";
 
-const getUserById = async (id, accessToken) => {
-  let user = {};
-
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${accessToken}`);
-
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
+const getUserById = async (token, id) => {
+  let config = {
+    maxBodyLength: Infinity,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   };
 
-  await fetch(`http://localhost:8080/api/v1/users/${id}`, requestOptions)
-    .then((response) => (user = response.json()))
-    .catch((error) => console.log(error));
-
-  return user;
+  try {
+    const res = await axios.get(
+      `http://localhost:8080/api/v1/users/${id}`,
+      config
+    );
+    if (res && res.data) {
+      return res.data;
+    }
+  } catch (error) {
+    return error.response;
+  }
 };
 
 const getUserByProfile = async (accessToken) => {
